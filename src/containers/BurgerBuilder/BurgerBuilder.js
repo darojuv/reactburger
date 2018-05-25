fc3597a84ec9875bbd5e65fb421c4279439698cd
Venu Daroju <darojuv@mail.com>
2018-05-24 22:03:48 -0700

    Till 129 Lecture

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -68,6 +68,12 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({purchasing: true});
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+    purchaseContinueHandler = () => {
+        alert('You continued !');
+    }
     render(){
         const disableInfo = {
             ...this.state.ingredients
@@ -77,8 +83,12 @@ class BurgerBuilder extends Component {
         }
         return(
             <Aux>   
-                <Modal show={this.state.purchasing} >
-                    <OrderSummary ingredients={this.state.ingredients} />
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
+                    <OrderSummary 
+                    ingredients={this.state.ingredients}
+                    purchaseCancelled = {this.purchaseCancelHandler}
+                    purchaseContinued = {this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
