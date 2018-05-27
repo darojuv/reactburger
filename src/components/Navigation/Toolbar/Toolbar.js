b2fc31898b4824fca57e8580941a8b805cbe0b38
darojuv <darojuv@gmail.com>
2018-05-27 10:53:51 -0700

    Till 139 lecture

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import classes from './Toolbar.css';
-//import Logo from '../../LOGO/Logo';
-import Toggle from '../SideDrawer/Toggle/Toggle';
+import Logo from '../../LOGO/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 import NavigationItems from '../NavigationItems/NavigationItems.js';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        {/* <div className={classes.Logo}>
+        <DrawerToggle clicked={props.drawerToggleClicked}  />
+        <div className={classes.Logo}>
             <Logo />
-        </div> */}
-        <Toggle />
+        </div> 
+
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
