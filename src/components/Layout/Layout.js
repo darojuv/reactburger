3f3334230002a65ed84ba129ec346e83d65d0d8b
Venu Daroju <darojuv@mail.com>
2018-05-27 06:38:54 -0700

    trying 138 Lecture

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-); 
-export default layout;
+class Layout extends Component{
+    state = {
+        showSideDrawer: true
+    }
+    SideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer: false});
+    }
+
+    render(){
+        return (
+            <Aux>
+            <Toolbar />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        );
+    }
+} 
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import classes from './Toolbar.css';
-import Logo from '../../LOGO/Logo';
+//import Logo from '../../LOGO/Logo';
+import Toggle from '../SideDrawer/Toggle/Toggle';
+
 import NavigationItems from '../NavigationItems/NavigationItems.js';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div className={classes.Logo}>
+        {/* <div className={classes.Logo}>
             <Logo />
-        </div>
-        <nav>
+        </div> */}
+        <Toggle />
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
