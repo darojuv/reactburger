3380d43592c39dcc55a358367f6fa637810d2bf8
darojuv <darojuv@gmail.com>
2019-03-15 03:33:52 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,72 +1,15 @@
 import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
-import { connect } from 'react-redux';
-import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
-import CheckOut from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Logout from './containers/Auth/Logout/Logout';
-import * as actions from './store/actions/index';
-//import asyncComponent from './hoc/asyncComponent/asyncComponent';
-
-// const asyncCheckOut = asyncComponent(() => {
-//   return import('./containers/Checkout/Checkout');
-// });
-// const asyncOrders = asyncComponent(() => {
-//   return import('./containers/Orders/Orders');
-// });
-// const asyncAuth = asyncComponent(() => {
-//   return import('./containers/Auth/Auth');
-// });
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
-  componentDidMount(){
-    this.props.onTryAutoSignin();
-  }
-
   render() {
-    let routes =(      
-      <Switch>
-        <Route path="/auth" component={Auth} />
-        <Route path="/" exact component={BurgerBuilder} />
-        <Redirect to="/" />
-      </Switch>
-    );
-    if(this.props.isAuthenticated){
-    routes =(
-      <Switch>
-        <Route path="/logout" component={Logout} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/checkout" component={CheckOut} />
-        <Route path="/orders" component={Orders} />
-        <Route path="/" exact component={BurgerBuilder} />
-        <Redirect to="/" />
-      </Switch>
-    )}
-      return (
+    return (
       <div >
         <Layout>
-          <Switch>
-            {routes}
-          </Switch>
-        </Layout>
+          <p>Test</p>
+          </Layout>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return{
-      isAuthenticated: state.auth.idToken != null
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSignin: () => dispatch(actions.authCheckState())
-  }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-//import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import bugerBuilderReducer from './store/reducers/burgerBuilder';
-import orderReducer from './store/reducers/order';
-import authReducer from './store/reducers/auth';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
-const rootReducer = combineReducers({
-    burgerBuilder: bugerBuilderReducer,
-    order: orderReducer,
-    auth: authReducer
-});
-
-const reduxStore = createStore(rootReducer/* preloadedState, */
-    , composeEnhancers( applyMiddleware(thunk)
-));
-
-const app = (
-                <Provider store={reduxStore}>
-                    <BrowserRouter >
-                        <App/>
-                    </BrowserRouter>
-                </Provider>
-            );
-
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
